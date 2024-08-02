b1080c9a18980beb65b81b83534d574154c6f8a0
Papireddy <papireddyeppala903@gmail.com>
2024-08-02 19:29:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+class Details(BaseModel):
+    data : List[str]
+
+@app.post("/bfhl")
+async def post_details(request : Details):
+    user_id = "john_doe_17091999"
+    email = "john@xyz.com"
+    roll_number = "ABCD123"
+    numbers = []
+    alphabets = []
+
+    for item in request.data:
+        if item.isdigit():
+            numbers.append(item)
+        elif item.isalpha():
+            alphabets.append(item)
+
+    highest_alphabet = []
+    if alphabets:
+        highest_alphabet_char = alphabets[0]
+        for char in alphabets:
+            if char.lower() > highest_alphabet_char.lower():
+                highest_alphabet_char = char
+        highest_alphabet = [highest_alphabet_char]
+
+    return {
+        "is_success": True,
+        "user_id": user_id,
+        "email": email,
+        "roll_number": roll_number,
+        "numbers": numbers,
+        "alphabets": alphabets,
+        "highest_alphabet": highest_alphabet
+    }
+
+
+@app.get("/bfhl")
+async def get_details():
+    return { "operation_code " : 1}
